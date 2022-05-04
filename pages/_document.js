ba4e8dbe69706a3c29e23c93c9ffa02f7247d88c
Selena Groh <sgroh@wayfair.com>
2022-05-03 23:04:41 -0400

    move theme file to src directory

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
-import chakraTheme from "./theme";
+import chakraTheme from "../src/theme";
 
 export default class MyDocument extends Document {
   render() {

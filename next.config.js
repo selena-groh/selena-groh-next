e0fcf3a58d19e5d70dcd5cbb5e9efaa67d2a0bd2
Selena Groh <sgroh@wayfair.com>
2022-05-24 23:31:06 -0400

    add redirects so that old links to selenagroh.com projects still work

--- next.config.js
+++ next.config.js
@@ -1,1 +1,25 @@
-module.exports = { images: { domains: ["images.ctfassets.net"] } };
+module.exports = {
+  images: { domains: ["images.ctfassets.net"] },
+  async redirects() {
+    const githubProjectURL = "https://selena-groh.github.io/";
+
+    const githubProjects = [
+      "wordle",
+      "guess-who-anne",
+      "guess-who-stardew",
+      "spelling-bee",
+      "dadlands",
+      "pokemon",
+      "gender-in-shakespeare",
+      "hierarchy-linking",
+      "drought-map",
+      "comp20-sgroh",
+    ];
+
+    return githubProjects.map((project) => ({
+      source: `/${project}`,
+      destination: `${githubProjectURL}${project}`,
+      permanent: false,
+    }));
+  },
+};

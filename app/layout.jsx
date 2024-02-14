f325d026f7c37f9e47f4be4c593a801bea0b113f
Selena Groh <selena.groh@gmail.com>
2024-02-13 21:20:43 -0500

    remove comments

--- app/layout.jsx
+++ app/layout.jsx
@@ -29,33 +29,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-// import Document, { Html, Head, Main, NextScript } from "next/document";
-
-// export default class MyDocument extends Document {
-//   render() {
-//     return (
-//       <Html lang="en">
-//         <Head>
-//           <link rel="preconnect" href="https://fonts.googleapis.com" />
-//           <link
-//             rel="preconnect"
-//             href="https://fonts.gstatic.com"
-//             crossOrigin=""
-//           />
-//           <link
-//             href="https://fonts.googleapis.com/css2?family=Roboto+Serif:opsz,wght@8..144,100;8..144,400;8..144,600&family=Roboto:300,400,500,700&display=swap"
-//             rel="stylesheet"
-//           />
-//         </Head>
-//         <body>
-//           <ColorModeScript
-//             initialColorMode={chakraTheme.config.initialColorMode}
-//           />
-//           <Main />
-//           <NextScript />
-//         </body>
-//       </Html>
-//     );
-//   }
-// }
